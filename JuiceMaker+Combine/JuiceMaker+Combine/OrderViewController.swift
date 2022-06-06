d16b4ee5c97da78d7f45a1923131834c2afa8630
dudu <whqtkf12@naver.com>
2022-06-06 21:27:11 +0900

    init: 프로젝트 파일 생성 및 스토리보드 삭제

---
+++ JuiceMaker+Combine/JuiceMaker+Combine/OrderViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  JuiceMaker+Combine
+//
+//  Created by 김도연 on 2022/06/06.
+//
+
+import UIKit
+
+final class OrderViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
